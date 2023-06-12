02f6c6a5f2ed7cc3662cbdfb67dac4e3d8634829
Son <sontruong02003@gmail.com>
2023-06-12 23:13:32 +0700

    update search patient

--- DoAn/Forms/FormSearchPatient.Designer.cs
+++ DoAn/Forms/FormSearchPatient.Designer.cs
@@ -30,14 +30,20 @@
         {
             this.lblListPatient = new System.Windows.Forms.Label();
             this.dGVListPatient = new System.Windows.Forms.DataGridView();
+            this.txtName = new MaterialSkin.Controls.MaterialTextBox2();
+            this.label1 = new System.Windows.Forms.Label();
             this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DateExamine = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Sex = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Birth = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.txtName = new MaterialSkin.Controls.MaterialTextBox2();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Symptom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Disease = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dpDate = new RJCodeAdvance.RJControls.RJDatePicker();
+            this.lblDateExam = new System.Windows.Forms.Label();
+            this.btnReset = new RJCodeAdvance.RJControls.RJButton();
+            this.txtTrieuChung = new MaterialSkin.Controls.MaterialTextBox2();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbbLBenh = new RJCodeAdvance.RJControls.RJComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dGVListPatient)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +51,7 @@
             // 
             this.lblListPatient.AutoSize = true;
             this.lblListPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.lblListPatient.Location = new System.Drawing.Point(77, 157);
+            this.lblListPatient.Location = new System.Drawing.Point(77, 183);
             this.lblListPatient.Name = "lblListPatient";
             this.lblListPatient.Size = new System.Drawing.Size(278, 31);
             this.lblListPatient.TabIndex = 7;
@@ -63,9 +69,8 @@
             this.Index,
             this.LastName,
             this.DateExamine,
-            this.Sex,
-            this.Birth,
-            this.Address});
+            this.Symptom,
+            this.Disease});
             this.dGVListPatient.Location = new System.Drawing.Point(83, 235);
             this.dGVListPatient.Name = "dGVListPatient";
             this.dGVListPatient.ReadOnly = true;
@@ -73,54 +78,6 @@
             this.dGVListPatient.RowTemplate.Height = 24;
             this.dGVListPatient.Size = new System.Drawing.Size(947, 382);
             this.dGVListPatient.TabIndex = 6;
-            // 
-            // Index
-            // 
-            this.Index.HeaderText = "Mã bệnh nhân";
-            this.Index.MinimumWidth = 6;
-            this.Index.Name = "Index";
-            this.Index.ReadOnly = true;
-            this.Index.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            // 
-            // LastName
-            // 
-            this.LastName.HeaderText = "Họ và tên";
-            this.LastName.MinimumWidth = 6;
-            this.LastName.Name = "LastName";
-            this.LastName.ReadOnly = true;
-            this.LastName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            // 
-            // DateExamine
-            // 
-            this.DateExamine.HeaderText = "Ngày khám";
-            this.DateExamine.MinimumWidth = 6;
-            this.DateExamine.Name = "DateExamine";
-            this.DateExamine.ReadOnly = true;
-            // 
-            // Sex
-            // 
-            this.Sex.FillWeight = 70F;
-            this.Sex.HeaderText = "Giới tính";
-            this.Sex.MinimumWidth = 6;
-            this.Sex.Name = "Sex";
-            this.Sex.ReadOnly = true;
-            this.Sex.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            // 
-            // Birth
-            // 
-            this.Birth.HeaderText = "Ngày sinh";
-            this.Birth.MinimumWidth = 6;
-            this.Birth.Name = "Birth";
-            this.Birth.ReadOnly = true;
-            this.Birth.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            // 
-            // Address
-            // 
-            this.Address.HeaderText = "Địa chỉ";
-            this.Address.MinimumWidth = 6;
-            this.Address.Name = "Address";
-            this.Address.ReadOnly = true;
-            this.Address.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // txtName
             // 
@@ -132,7 +89,7 @@
             this.txtName.HideSelection = true;
             this.txtName.Hint = "Nhập họ và tên";
             this.txtName.LeadingIcon = null;
-            this.txtName.Location = new System.Drawing.Point(213, 53);
+            this.txtName.Location = new System.Drawing.Point(222, 36);
             this.txtName.MaxLength = 32767;
             this.txtName.MouseState = MaterialSkin.MouseState.OUT;
             this.txtName.Name = "txtName";
@@ -157,11 +114,174 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.label1.Location = new System.Drawing.Point(77, 53);
+            this.label1.Location = new System.Drawing.Point(70, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(130, 31);
             this.label1.TabIndex = 8;
             this.label1.Text = "Họ và tên";
+            // 
+            // Index
+            // 
+            this.Index.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Index.HeaderText = "STT";
+            this.Index.MinimumWidth = 6;
+            this.Index.Name = "Index";
+            this.Index.ReadOnly = true;
+            this.Index.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Index.Width = 63;
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.LastName.HeaderText = "Họ và tên";
+            this.LastName.MinimumWidth = 6;
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            this.LastName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // DateExamine
+            // 
+            this.DateExamine.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DateExamine.HeaderText = "Ngày khám";
+            this.DateExamine.MinimumWidth = 6;
+            this.DateExamine.Name = "DateExamine";
+            this.DateExamine.ReadOnly = true;
+            // 
+            // Symptom
+            // 
+            this.Symptom.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Symptom.FillWeight = 70F;
+            this.Symptom.HeaderText = "Triệu chứng";
+            this.Symptom.MinimumWidth = 6;
+            this.Symptom.Name = "Symptom";
+            this.Symptom.ReadOnly = true;
+            this.Symptom.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Disease
+            // 
+            this.Disease.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Disease.HeaderText = "Loại bệnh";
+            this.Disease.MinimumWidth = 6;
+            this.Disease.Name = "Disease";
+            this.Disease.ReadOnly = true;
+            this.Disease.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // dpDate
+            // 
+            this.dpDate.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dpDate.BorderSize = 3;
+            this.dpDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dpDate.CustomFormat = "dd/MM/yyyy";
+            this.dpDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.dpDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dpDate.Location = new System.Drawing.Point(745, 19);
+            this.dpDate.MinimumSize = new System.Drawing.Size(4, 60);
+            this.dpDate.Name = "dpDate";
+            this.dpDate.Size = new System.Drawing.Size(272, 60);
+            this.dpDate.SkinColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(247)))), ((int)(((byte)(218)))));
+            this.dpDate.TabIndex = 11;
+            this.dpDate.TextColor = System.Drawing.Color.Black;
+            this.dpDate.ValueChanged += new System.EventHandler(this.dpDate_ValueChanged);
+            // 
+            // lblDateExam
+            // 
+            this.lblDateExam.AutoSize = true;
+            this.lblDateExam.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.lblDateExam.Location = new System.Drawing.Point(588, 36);
+            this.lblDateExam.Name = "lblDateExam";
+            this.lblDateExam.Size = new System.Drawing.Size(151, 31);
+            this.lblDateExam.TabIndex = 10;
+            this.lblDateExam.Text = "Ngày khám";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnReset.BackgroundColor = System.Drawing.Color.SeaGreen;
+            this.btnReset.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnReset.BorderRadius = 10;
+            this.btnReset.BorderSize = 0;
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(880, 649);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(150, 40);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "Reset";
+            this.btnReset.TextColor = System.Drawing.Color.White;
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // txtTrieuChung
+            // 
+            this.txtTrieuChung.AnimateReadOnly = false;
+            this.txtTrieuChung.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtTrieuChung.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtTrieuChung.Depth = 0;
+            this.txtTrieuChung.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTrieuChung.HideSelection = true;
+            this.txtTrieuChung.Hint = "Nhập triệu chứng";
+            this.txtTrieuChung.LeadingIcon = null;
+            this.txtTrieuChung.Location = new System.Drawing.Point(222, 118);
+            this.txtTrieuChung.MaxLength = 32767;
+            this.txtTrieuChung.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtTrieuChung.Name = "txtTrieuChung";
+            this.txtTrieuChung.PasswordChar = '\0';
+            this.txtTrieuChung.PrefixSuffixText = null;
+            this.txtTrieuChung.ReadOnly = false;
+            this.txtTrieuChung.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtTrieuChung.SelectedText = "";
+            this.txtTrieuChung.SelectionLength = 0;
+            this.txtTrieuChung.SelectionStart = 0;
+            this.txtTrieuChung.ShortcutsEnabled = true;
+            this.txtTrieuChung.Size = new System.Drawing.Size(287, 36);
+            this.txtTrieuChung.TabIndex = 14;
+            this.txtTrieuChung.TabStop = false;
+            this.txtTrieuChung.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtTrieuChung.TrailingIcon = null;
+            this.txtTrieuChung.UseSystemPasswordChar = false;
+            this.txtTrieuChung.UseTallSize = false;
+            this.txtTrieuChung.TextChanged += new System.EventHandler(this.txtTrieuChung_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label2.Location = new System.Drawing.Point(70, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 31);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Triệu chứng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label3.Location = new System.Drawing.Point(594, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 31);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Loại bệnh";
+            // 
+            // cbbLBenh
+            // 
+            this.cbbLBenh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(247)))), ((int)(((byte)(218)))));
+            this.cbbLBenh.BorderColor = System.Drawing.Color.Gray;
+            this.cbbLBenh.BorderSize = 1;
+            this.cbbLBenh.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.cbbLBenh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cbbLBenh.ForeColor = System.Drawing.Color.Black;
+            this.cbbLBenh.IconColor = System.Drawing.Color.MediumSlateBlue;
+            this.cbbLBenh.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+            this.cbbLBenh.ListTextColor = System.Drawing.Color.DimGray;
+            this.cbbLBenh.Location = new System.Drawing.Point(745, 118);
+            this.cbbLBenh.MinimumSize = new System.Drawing.Size(200, 30);
+            this.cbbLBenh.Name = "cbbLBenh";
+            this.cbbLBenh.Padding = new System.Windows.Forms.Padding(1);
+            this.cbbLBenh.Size = new System.Drawing.Size(272, 30);
+            this.cbbLBenh.TabIndex = 76;
+            this.cbbLBenh.Texts = "";
+            this.cbbLBenh.OnSelectedIndexChanged += new System.EventHandler(this.cbbLBenh_OnSelectedIndexChanged);
             // 
             // FormSearchPatient
             // 
@@ -169,6 +289,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(247)))), ((int)(((byte)(218)))));
             this.ClientSize = new System.Drawing.Size(1084, 717);
+            this.Controls.Add(this.cbbLBenh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTrieuChung);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.dpDate);
+            this.Controls.Add(this.lblDateExam);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblListPatient);
@@ -190,8 +317,14 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Index;
         private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
         private System.Windows.Forms.DataGridViewTextBoxColumn DateExamine;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Sex;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Birth;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Symptom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Disease;
+        private RJCodeAdvance.RJControls.RJDatePicker dpDate;
+        private System.Windows.Forms.Label lblDateExam;
+        private RJCodeAdvance.RJControls.RJButton btnReset;
+        private MaterialSkin.Controls.MaterialTextBox2 txtTrieuChung;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private RJCodeAdvance.RJControls.RJComboBox cbbLBenh;
     }
 }
--- DoAn/Forms/FormSearchPatient.cs
+++ DoAn/Forms/FormSearchPatient.cs
@@ -6,6 +6,8 @@ namespace DoAn.Forms
 {
     public partial class FormSearchPatient : Form
     {
+        private bool isDateChange = false;
+
         public FormSearchPatient()
         {
             InitializeComponent();
@@ -17,57 +19,115 @@ namespace DoAn.Forms
             dGVListPatient.Rows.Clear();
             using (var db = new DataPKEntities())
             {
-                var select = (from s in db.PHIEUKHAMs
-                              group s by s.MaBenhNhan into g
-                              select g.FirstOrDefault()).ToList();
+                //var select = (from s in db.PHIEUKHAMs
+                //              group s by s.MaBenhNhan into g
+                //              select g.FirstOrDefault()).ToList();
+                var select = (from s in db.PHIEUKHAMs select s).ToList();
+
+                int stt = 0;
                 foreach (var person in select)
                 {
+                    stt++;
                     DataGridViewRow row = new DataGridViewRow();
                     row.CreateCells(dGVListPatient);
-                    row.Cells[dGVListPatient.Columns["Index"].Index].Value = person.MaBenhNhan;
+                    row.Cells[dGVListPatient.Columns["Index"].Index].Value = stt;
                     row.Cells[dGVListPatient.Columns["LastName"].Index].Value = person.BENHNHAN.HoTen;
-                    row.Cells[dGVListPatient.Columns["Sex"].Index].Value = person.BENHNHAN.GioiTinh;
-                    row.Cells[dGVListPatient.Columns["Birth"].Index].Value = person.BENHNHAN.NamSinh.Value.ToString("dd'/'MM'/'yyyy");
-                    row.Cells[dGVListPatient.Columns["Address"].Index].Value = person.BENHNHAN.DiaChi;
+                    row.Cells[dGVListPatient.Columns["Disease"].Index].Value = person.LOAIBENH.TenLoaiBenh;
+                    row.Cells[dGVListPatient.Columns["Symptom"].Index].Value = person.TrieuChung;
                     row.Cells[dGVListPatient.Columns["DateExamine"].Index].Value = person.NgayKham.Value.ToString("dd'/'MM'/'yyyy");
                     // person.NgayKham.Value.ToString("dd'/'MM'/'yyyy");
                     dGVListPatient.Rows.Add(row);
                 }
 
+                var loaibenh = from s in db.LOAIBENHs
+                               select s;
+                foreach (var item in loaibenh)
+                {
+                    cbbLBenh.Items.Add(item.TenLoaiBenh);
+                }
+                cbbLBenh.AutoCompleteMode = AutoCompleteMode.Suggest;
+                cbbLBenh.AutoCompleteSource = AutoCompleteSource.ListItems;
             }
+
+
         }
         private void btnSearch_Click(object sender, EventArgs e)
         {
 
         }
-        private void txtName_TextChanged(object sender, EventArgs e)
+        private void anyChanged()
         {
             string lastName = txtName.Text;
+            string trieuchung = txtTrieuChung.Text;
+            DateTime selectedDate = dpDate.Value.Date;
+            string loaibenh = cbbLBenh.SelectedItem.ToString();
             using (var db = new DataPKEntities())
             {
                 var select = (from s in db.PHIEUKHAMs
                               where s.BENHNHAN.HoTen.Contains(lastName)
-                              group s by s.MaBenhNhan into g
-                              select g.FirstOrDefault()).ToList();
+                              && s.TrieuChung.Contains(trieuchung)
+                              && s.LOAIBENH.TenLoaiBenh.Contains(loaibenh)
+                              select s).ToList();
 
                 dGVListPatient.Rows.Clear(); // Clear the existing rows in the DataGridView
 
+                int stt = 0;
                 foreach (var person in select)
                 {
+                    if (isDateChange)
+                    {
+                        if (!(person.NgayKham.Value.Year == selectedDate.Year
+                              && person.NgayKham.Value.Month == selectedDate.Month
+                              && person.NgayKham.Value.Day == selectedDate.Day))
+                            continue;
+                    }
+                    stt++;
                     DataGridViewRow row = new DataGridViewRow();
                     row.CreateCells(dGVListPatient);
-                    row.Cells[dGVListPatient.Columns["Index"].Index].Value = person.MaBenhNhan;
+                    row.Cells[dGVListPatient.Columns["Index"].Index].Value = stt;
                     row.Cells[dGVListPatient.Columns["LastName"].Index].Value = person.BENHNHAN.HoTen;
-                    row.Cells[dGVListPatient.Columns["Sex"].Index].Value = person.BENHNHAN.GioiTinh;
-                    row.Cells[dGVListPatient.Columns["Birth"].Index].Value = person.BENHNHAN.NamSinh.Value.ToString("dd'/'MM'/'yyyy");
-                    row.Cells[dGVListPatient.Columns["Address"].Index].Value = person.BENHNHAN.DiaChi;
+                    row.Cells[dGVListPatient.Columns["Disease"].Index].Value = person.LOAIBENH.TenLoaiBenh;
+                    row.Cells[dGVListPatient.Columns["Symptom"].Index].Value = person.TrieuChung;
+                    row.Cells[dGVListPatient.Columns["DateExamine"].Index].Value = person.NgayKham.Value.ToString("dd'/'MM'/'yyyy");
+                    // person.NgayKham.Value.ToString("dd'/'MM'/'yyyy");
                     dGVListPatient.Rows.Add(row);
                 }
             }
         }
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            anyChanged();
+        }
         private void btnReset_Click(object sender, EventArgs e)
         {
+
             InitializeData();
+        }
+
+        private void dpDate_ValueChanged(object sender, EventArgs e)
+        {
+            isDateChange = true;
+            anyChanged();
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            txtName.Text = null;
+            isDateChange = false;
+            dpDate.Value = DateTime.Now;
+            cbbLBenh.Items.Clear();
+            cbbLBenh.Texts = null;
+            InitializeData();
+        }
+
+        private void txtTrieuChung_TextChanged(object sender, EventArgs e)
+        {
+            anyChanged();
+        }
+
+        private void cbbLBenh_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            anyChanged();
         }
     }
 }
